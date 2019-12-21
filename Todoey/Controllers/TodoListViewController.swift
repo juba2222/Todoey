007e35e0a4b917925ca8bd253c7b4b2c6e38e48c
yousef omar <zeef059@gmail.com>
2019-12-21 19:15:17 +0200

    resisting  data  useing coddle protocol and plist

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-    var itemArray = ["finde mike ","buy eggs ","go to school"]
+    var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
+
+        //= ["finde mike ","buy eggs ","go to school"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadItem()
+        
     }
      
     //MARK: TableView Datasource
@@ -21,29 +26,32 @@ class TodoListViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done == true ? .checkmark : .none
         return cell
     }
     
     //MARK: TableView Delegat
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //check mark: select and deselect
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveData()
         // selection background
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // when select add Item
-            self.itemArray.append(textField.text!)
-            self.tableView.reloadData()
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            self.saveData()
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Creat new Item"
@@ -51,6 +59,28 @@ class TodoListViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+
+    }
+     func saveData() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        } catch  {
+            print("error incoding item array \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    func loadItem() {
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self,from: data)
+            } catch  {
+                print("error decoding item array \(error)")
+            }
+        }
     }
 }
 // !!!: ...
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by yousef Omar on 12/21/19.
+//  Copyright © 2019 yousef Omar. All rights reserved.
+//
+
+import Foundation
+class Item:Codable {
+    var title:String = ""
+    var done:Bool = false
+}
